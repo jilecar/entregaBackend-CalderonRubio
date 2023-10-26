5db4266827e4abfd0d069ab413ed3c3ccb915850
jilecar <jimmy.calderon86@gmail.com>
2023-10-25 21:24:00 -0500

    organizacion de archivos

---
+++ src/app.js
@@ -0,0 +1,14 @@
+//Archivo principal que inicia el servidor Express
+const express = require('express');
+const app = express();
+const productsRouter = require('./products');
+const cartsRouter = require('./carts');
+
+app.use(express.json());
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
+
+const port = 8080;
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
